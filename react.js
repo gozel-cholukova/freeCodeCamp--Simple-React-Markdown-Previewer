bc6221d7a756640fd17c3296df3cf4995e11b24a
Gozel Cholukova <checkboxtm@gmail.com>
2021-02-16 00:13:42 +0500

    Update react.js

--- react.js
+++ react.js
@@ -1,33 +1,80 @@
+const initialState = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`;
+
 class App extends React.Component {
   state = {
-    markdown: ''
-  };
+    text: initialState
+  }
   
   handleChange = (e) => {
     this.setState({
-      markdown: e.target.value
-    });
+      text: e.target.value
+    })
   }
   
   render() {
-    const { markdown } = this.state;
+    const { text } = this.state;  
     
+    const markdown = marked(text);
     return (
       <div>
-        <h1 className="text-center mt-4">Convert your Markdown</h1>
+        <h2 className="text-center m-4">Convert your Markdown</h2>
         <div className="row">
           <div className="col-6">
-            <h5>Enter your markdown here:</h5>
-            <textarea className="form-control" id="editor" value={markdown} onChange={this.handleChange}/>
+            <h6 className="text-center">Enter your markdown here:</h6>
+            <textarea className="form-control p-2" id="editor" value={text} onChange={this.handleChange}/>
           </div>
           
-          <div className="col-6 preview" id="preview">
-              <h5>See the result:</h5>
-            <p>Hello</p>
+          <div className="col-6" id="preview">
+              <h6 className="text-center">See the result:</h6>
+              <div className="preview rounded p-2" dangerouslySetInnerHTML={{__html: markdown}} />            
+          </div>
         </div>
       </div>
-    );
+    )
   }
-}
+};
 
 ReactDOM.render(<App/>, document.getElementById('app'));
